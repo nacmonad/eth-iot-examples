f5e8576853de85c6d01c61bd19785268ce36de4f
Scott <macnomad84@gmail.com>
2017-08-07 22:16:26 -0400

    Update index.js

--- pi/index.js
+++ pi/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import switchExample from './examples/switchExample';
+import switchExample from './examples/blinkExample';
 
 const PORT = 3030;
 
 let app = new express();
 app.set('port', PORT)
 
-app = switchExample(app);
+app = blinkExample(app);
 
 app.listen(app.get('port'), ()=>{
   console.log(`PI-Node on port ${app.get('port')}`);
