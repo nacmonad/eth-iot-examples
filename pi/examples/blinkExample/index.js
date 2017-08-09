629fbefaa556d8a0ec8954beb77ba8b4874c9cf4
nacmonad <macnomad84@gmail.com>
2017-08-08 20:35:11 -0400

    where we at...

--- pi/examples/blinkExample/index.js
+++ pi/examples/blinkExample/index.js
@@ -1,6 +1,9 @@
 import RpiLeds from 'rpi-leds';
 import Web3 from 'web3';
 
+//import gpio from 'rpi-gpio';
+import gpio from 'gpio';
+
 export default (app) => {
   console.log("Hi blinkExample")
   const leds = new RpiLeds();
@@ -16,7 +19,6 @@ export default (app) => {
   var ABI = JSON.parse('[{"constant":true,"inputs":[],"name":"getData","outputs":[{"name":"retData","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"inData","type":"uint256"}],"name":"setData","outputs":[],"payable":false,"type":"function"},{"anonymous":false,"inputs":[{"indexed":false,"name":"msgData","type":"uint256"}],"name":"blinkEvent","type":"event"}]');
 
   const blinkContract = web3.eth.contract(ABI).at(contractAddress);
-
 
   blinkContract.blinkEvent({}, (error, msg)=> {
     if(!error) {
@@ -48,6 +50,7 @@ export default (app) => {
       clearInterval(iv);
     }, 10000)
   }
+
 
   return app;
 }
--- pi/index.js
+++ pi/index.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import switchExample from './examples/switchExample';
+//import example from './examples/blinkExample';
+import example from './examples/switchExample';
+//import example from './examples/erc20Example';
 
 const PORT = 3030;
 
 let app = new express();
 app.set('port', PORT)
 
-app = switchExample(app);
+app = example(app);
 
 app.listen(app.get('port'), ()=>{
   console.log(`PI-Node on port ${app.get('port')}`);
